626207422b2f475a83321342ed32d9dded850eba
ghao001 <36051327+ghao001@users.noreply.github.com>
2018-07-08 00:23:29 -0700

    Update Solution.java

--- Solution.java
+++ Solution.java
@@ -31,11 +31,11 @@ class Solution {
                 if(in.charAt(j)>in.charAt(j+1))
                 {
                     if(j==0)
-                        in=in.charAt(1)+in.charAt(0)+in.substring(2);
+                        in=in.substring(1,2)+in.substring(0,1)+in.substring(2);
                     else 
                         if(j==in.length()-1)
-                            in=in.substring(0,j-1)+in.charAt(j+1)+in.charAt(j);    
-                        else in=in.substring(0,j-1)+in.charAt(j+1)+in.charAt(j)+in.substring(j+2);
+                            in=in.substring(0,j)+in.substring(j+1,j+2)+in.substring(j,j+1);    
+                        else in=in.substring(0,j)+in.substring(j+1,j+2)+in.substring(j,j+1)+in.substring(j+2);
                 }
             }
         }
